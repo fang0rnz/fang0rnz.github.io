2ac20bbed74fdb689dee1201cf8c98623be7176a
Lucas Sartori M <lucas.sartm@gmail.com>
2017-09-29 10:08:26 -0300

    added lgallery

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-$(document).ready(function() {
-  // This will create a single gallery from all elements that have class "gallery-item"
-  $('.gallery-item').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true,
-    },
-  });
+lightGallery(document.getElementById('lightgallery'), {
+  selector: '.gallery-item',
 });
