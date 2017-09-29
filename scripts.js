2cb93e92608926e92894db7b22283ca9123dcc30
Lucas Sartori M <lucas.sartm@gmail.com>
2017-09-29 11:12:51 -0300

    hover tests branch

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 lightGallery(document.getElementById('lightgallery'), {
   selector: '.gallery-item',
 });
+
+var items = $('.single-portfolio');
+
+$.each(items, function(key, value) {
+  console.warn(
+    $(value)
+      .find('a')
+      .after()
+  );
+  // $(value)
+  //   .find('a:after')
+  //   .css('content', $(value).data('title'));
+});
